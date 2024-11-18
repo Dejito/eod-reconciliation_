523a76995c08fc4ef8851611989dc8e1656b05bf
dejito <Deerealboy@gmail.com>
2024-11-18 21:20:29 +0100

    nav to history screen logic added

--- lib/screens/history/history.dart
+++ lib/screens/history/history.dart
@@ -21,13 +21,11 @@ class _HistoryState extends State<History> {
 
   @override
   void didChangeDependencies() async {
-
     if(!isLoaded) {
       await Provider.of<TransactionBrain>(context, listen: false).fetchAndSetData();
       await Provider.of<PosWithdrawalBrain>(context,listen: false).fetchAndSetIncrease();
       await Provider.of<DepositBrain>(context, listen: false).fetchAndSetData();
       await Provider.of<ProfitDatabase>(context, listen: false).fetchAndSetData();
-      Navigator.of(context).pushNamed(History.id);
     }
     isLoaded = true;
     super.didChangeDependencies();
@@ -48,72 +46,68 @@ class _HistoryState extends State<History> {
       appBar: AppBar(
         title: const Text('History'),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.all(12),
-            child: FutureBuilder(
-              future: pos.fetchAndSetIncrease(),
-              builder: (context, snapshot) =>
-                  snapshot.connectionState == ConnectionState.waiting ||
-                          posDB.isEmpty
-                      ? const Center(
-                          child: Text('No transactions added yet!'),
-                        )
-                      : SingleChildScrollView(
-                          // reverse: true,
-                          controller: controller,
-                          // physics: NeverScrollableScrollPhysics(),
-                          child: ListView.builder(
-                            reverse: true,
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemBuilder: (context, i) {
-                              return Card(
-                                color: Colors.amberAccent.shade100,
-                                child: Container(
-                                    margin: const EdgeInsets.symmetric(
-                                        vertical: 5, horizontal: 10),
-                                    padding: const EdgeInsets.all(10),
-                                    child: Column(
-                                      children: [
-                                        Text(
-                                          posDB[i].id.substring(0, 11),
-                                          style: const TextStyle(
-                                              color: Colors.blue,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        Text(
-                                          'POS increase is ${posDB[i].increaseAmount.toString()}',
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        Text(
-                                          'Transfer withdrawal increase is  ${transferDb[i].increaseAmount.toString()}',
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        Text(
-                                          'Deposit increase is  ${depoDb[i].increaseAmount.toString()}',
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        Text(
-                                          'Total increase is  ${profitDb[i].increaseAmount.toString()}',
-                                          style: const TextStyle(
-                                              color: Colors.blue,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      ],
-                                    )),
-                              );
-                            },
-                            itemCount: pos.databaseTransactions.length,
-                          ),
-                        ),
-            ),
-          ),
-        ],
+      body: Container(
+        margin: const EdgeInsets.all(12),
+        child: FutureBuilder(
+          future: pos.fetchAndSetIncrease(),
+          builder: (context, snapshot) =>
+              snapshot.connectionState == ConnectionState.waiting ||
+                      posDB.isEmpty
+                  ? const Center(
+                      child: Text('No transactions added yet!'),
+                    )
+                  : SingleChildScrollView(
+                      // reverse: true,
+                      controller: controller,
+                      // physics: NeverScrollableScrollPhysics(),
+                      child: ListView.builder(
+                        reverse: true,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, i) {
+                          return Card(
+                            color: Colors.amberAccent.shade100,
+                            child: Container(
+                                margin: const EdgeInsets.symmetric(
+                                    vertical: 5, horizontal: 10),
+                                padding: const EdgeInsets.all(10),
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      posDB[i].id.substring(0, 11),
+                                      style: const TextStyle(
+                                          color: Colors.blue,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      'POS increase is ${posDB[i].increaseAmount.toString()}',
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      'Transfer withdrawal increase is  ${transferDb[i].increaseAmount.toString()}',
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      'Deposit increase is  ${depoDb[i].increaseAmount.toString()}',
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      'Total increase is  ${profitDb[i].increaseAmount.toString()}',
+                                      style: const TextStyle(
+                                          color: Colors.blue,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                )),
+                          );
+                        },
+                        itemCount: pos.databaseTransactions.length,
+                      ),
+                    ),
+        ),
       ),
     );
   }
