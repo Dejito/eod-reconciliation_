245bca04eccb074deb5b9078fb1791ae71fb9786
dejito <Deerealboy@gmail.com>
2024-11-18 09:34:51 +0100

    appbar implemented

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -37,10 +37,36 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
+        centerTitle: true,
+        // elevation: 1,
         title: const Text(
-          'Transaction Type'
+          'Homepage'
         ),
+        bottom:  PreferredSize(
+          preferredSize: const Size.fromHeight(5.0),
+          child: Container(
+            color: Colors.grey,
+            height: 2,
+          ),
+        ),
+        actions: [
+          InkWell(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: const Color(0xFF1E1E1E)
+                ),
+                borderRadius: const BorderRadius.all(Radius.circular(100))
+              ),
+              child: const Icon(
+                Icons.person
+              ),
+            ),
+          ),
+          const SizedBox(width: 12,)
+        ],
       ),
       drawer: const MainDrawer(),
       body: Column(
