3e1dc9f608666528706f34aba557ae8d30f21c3b
dejito <Deerealboy@gmail.com>
2024-11-18 11:10:37 +0100

    screen util added

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:eod_reconcilaton/screens/transfer_withdrawal/transfer_withdrawal
 import 'package:eod_reconcilaton/screens/transfer_withdrawal/tx_with_increase_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:eod_reconcilaton/screens/tab_screen.dart';
 
@@ -42,30 +43,34 @@ class MyApp extends StatelessWidget {
             value: ProfitDatabase()),
       ],
 
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        builder: EasyLoading.init(),
-        theme: ThemeData(
-          primaryColor: const Color(0xFF00066D),
-          fontFamily: 'Lato',
-          buttonTheme: ButtonTheme.of(context).copyWith(
-            buttonColor: Colors.blueAccent,
+      child: ScreenUtilInit(
+        minTextAdapt: true,
+        designSize: const Size(390, 884),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          builder: EasyLoading.init(),
+          theme: ThemeData(
+            primaryColor: const Color(0xFF00066D),
+            fontFamily: 'Lato',
+            buttonTheme: ButtonTheme.of(context).copyWith(
+              buttonColor: Colors.blueAccent,
+            ),
           ),
+          home: const LandingPage(),
+          routes: {
+            LandingPage.id: (context) => const LandingPage(),
+            Dashboard.id: (context) => const Dashboard(),
+            TransferWithdrawalScreen.id: (context) => const TransferWithdrawalScreen(),
+            TxWithIncreaseScreen.id: (context) => const TxWithIncreaseScreen(),
+            POSWithdrawalScreen.id: (context) =>  const POSWithdrawalScreen(),
+            PosWithIncreaseScreen.id: (context) =>  const PosWithIncreaseScreen(),
+            DepositScreen.id: (context) => const DepositScreen(),
+            DepositIncreaseScreen.id: (context) => const DepositIncreaseScreen(),
+            TotalProfit.id: (context) => const TotalProfit(),
+            History.id: (context) => const History(),
+            TabScreen.id: (context) => const TabScreen()
+          },
         ),
-        home: const LandingPage(),
-        routes: {
-          LandingPage.id: (context) => const LandingPage(),
-          Dashboard.id: (context) => const Dashboard(),
-          TransferWithdrawalScreen.id: (context) => const TransferWithdrawalScreen(),
-          TxWithIncreaseScreen.id: (context) => const TxWithIncreaseScreen(),
-          POSWithdrawalScreen.id: (context) =>  const POSWithdrawalScreen(),
-          PosWithIncreaseScreen.id: (context) =>  const PosWithIncreaseScreen(),
-          DepositScreen.id: (context) => const DepositScreen(),
-          DepositIncreaseScreen.id: (context) => const DepositIncreaseScreen(),
-          TotalProfit.id: (context) => const TotalProfit(),
-          History.id: (context) => const History(),
-          TabScreen.id: (context) => const TabScreen()
-        },
       ),
     );
   }
