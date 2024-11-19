59436687f473fd0eb7d2a3e4d004fa18c3889efe
dejito <Deerealboy@gmail.com>
2024-11-19 23:10:23 +0100

    login screen implementation

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,11 @@
 import 'package:eod_reconcilaton/screens/tab_screen.dart';
+import 'package:eod_reconcilaton/utils/assets.dart';
+import 'package:eod_reconcilaton/utils/widgets.dart';
 import 'package:eod_reconcilaton/widgets/loading_indicator.dart';
 import 'package:eod_reconcilaton/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   static const id = 'landing_page';
@@ -16,96 +20,99 @@ class LoginScreen extends StatelessWidget {
       //   ),
       // ),
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Container(
-            margin: const EdgeInsets.all(16),
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/landing_page.png',
-                  fit: BoxFit.fill,
-                ),
-                const Text(
-                  "EOD Reconciliation",
-                  style: TextStyle(
-                      color: Colors.blueAccent,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25),
-                ),
-                const SizedBox(
-                  height: 48,
-                ),
-                const TextField(
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
-                    hintText: "Enter your phone number",
-                    hintStyle: TextStyle(color: Colors.grey),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 30),
-                const TextField(
-                  obscureText: true,
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
-                    hintText: "Enter your pin",
-                    hintStyle: TextStyle(color: Colors.grey),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ),
-                ),
-
-                // Spacer(),
-
-                const SizedBox(height: 100),
-
-                buildElevatedButton(
-                    onPressed: () async {
-                      showLoadingIndicator();
-                      // await Future.delayed(const Duration(seconds: 2));
-                      dismissLoadingIndicator();
-                      if (context.mounted) {
-                        Navigator.of(context)
-                            .pushReplacementNamed(TabScreen.id);
-                      }
-                    },
-                    label: "Login",
-
-                    buttonColor: Colors.blueAccent.shade200,
-                    textColor: Colors.white,
-                    borderColor: Colors.transparent),
-                const SizedBox(
-                  height: 10,
-                ),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.alphabetic,
-                  children: [
-                    const Text("Don't have an account?"),
-                    TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          "Sign up",
-                          style: TextStyle(
-                              color: Colors.blueAccent,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15),
-                        )),
-                  ],
-                )
-              ],
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Image.asset(
+              //   'assets/images/landing_page.png',
+              //   fit: BoxFit.fill,
+              // ),
+              // titleText(
+              //   "EOD Reconciliation",
+              //   fontSize: 25.sp,
+              // ),
+          Text(
+            "Login to continue",
+            style: GoogleFonts.mitr(
+              fontSize: 25.sp,
+              // fontWeight: FontWeight.bold,
+              color: AppColors.primaryColor,
             ),
+          ),
+
+              const SizedBox(
+                height: 68,
+              ),
+              const TextField(
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  hintText: "Phone number",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              const TextField(
+                obscureText: true,
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  hintText: "Enter your pin",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
+
+              // Spacer(),
+
+              const SizedBox(height: 100),
+
+              buildElevatedButton(
+                  onPressed: () async {
+                    showLoadingIndicator();
+                    // await Future.delayed(const Duration(seconds: 2));
+                    dismissLoadingIndicator();
+                    if (context.mounted) {
+                      Navigator.of(context)
+                          .pushReplacementNamed(TabScreen.id);
+                    }
+                  },
+                  label: "Login",
+                  buttonColor: AppColors.primaryColor,
+                  textColor: Colors.white,
+                  borderColor: Colors.transparent),
+              const SizedBox(
+                height: 10,
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: [
+                  const Text("Don't have an account?"),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Sign up",
+                        style: TextStyle(
+                            color: Colors.blueAccent,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15),
+                      )),
+                ],
+              )
+            ],
           ),
         ),
       ),
