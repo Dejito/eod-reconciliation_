9a57a1141b213480c5f661fe49e4d2f3355ed814
dejito <Deerealboy@gmail.com>
2024-11-20 15:49:27 +0100

    sign up screen implementation started

--- lib/screens/sign_up/sign_up.dart
+++ lib/screens/sign_up/sign_up.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+
+      ],
+    );
+  }
+}
