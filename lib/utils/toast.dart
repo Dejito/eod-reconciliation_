c8bc62572d3bd14b04943be28e5d09edc1e199a9
dejito <Deerealboy@gmail.com>
2024-11-20 01:04:59 +0100

    started toast implementation

---
+++ lib/utils/toast.dart
@@ -0,0 +1,17 @@
+import "package:flutter/material.dart";
+import "package:fluttertoast/fluttertoast.dart";
+
+
+showToast({
+  required String message,
+  Color backgroundColor = Colors.black,
+  Color textColor = Colors.white,
+}) {
+  return Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: backgroundColor,
+      textColor: textColor
+  );
+}
